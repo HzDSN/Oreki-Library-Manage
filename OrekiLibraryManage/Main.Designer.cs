da831daaa17e67854ea8f3cff7ee00349e142b93
Oreki Dawson <dongsongning989@hotmail.com>
2017-12-18 22:35:15 +0800

    Add the main window.

---
+++ OrekiLibraryManage/Main.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace OrekiLibraryManage
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.图书借阅ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.用户管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.图书管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.员工管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.图书借阅ToolStripMenuItem,
+            this.用户管理ToolStripMenuItem,
+            this.图书管理ToolStripMenuItem,
+            this.员工管理ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(689, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 图书借阅ToolStripMenuItem
+            // 
+            this.图书借阅ToolStripMenuItem.Name = "图书借阅ToolStripMenuItem";
+            this.图书借阅ToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.图书借阅ToolStripMenuItem.Text = "图书借阅";
+            // 
+            // 用户管理ToolStripMenuItem
+            // 
+            this.用户管理ToolStripMenuItem.Name = "用户管理ToolStripMenuItem";
+            this.用户管理ToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.用户管理ToolStripMenuItem.Text = "用户管理";
+            // 
+            // 图书管理ToolStripMenuItem
+            // 
+            this.图书管理ToolStripMenuItem.Name = "图书管理ToolStripMenuItem";
+            this.图书管理ToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.图书管理ToolStripMenuItem.Text = "图书管理";
+            // 
+            // 员工管理ToolStripMenuItem
+            // 
+            this.员工管理ToolStripMenuItem.Name = "员工管理ToolStripMenuItem";
+            this.员工管理ToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.员工管理ToolStripMenuItem.Text = "员工管理";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 588);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "欧乐可图书馆管理系统";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 图书借阅ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 用户管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 图书管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 员工管理ToolStripMenuItem;
+    }
+}
